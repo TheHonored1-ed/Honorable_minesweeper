e474872fc564602eb294f017e861295fcb8375ab
TheHonored1-ed <abner.jiksa@icloud.com>
2026-03-22 21:16:12 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'honorable-minesweeper-cache-v1';
+const FILES_TO_CACHE = ['./index.html','./manifest.json','./sw.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k=>{if(k!==CACHE_NAME) return caches.delete(k);}))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
